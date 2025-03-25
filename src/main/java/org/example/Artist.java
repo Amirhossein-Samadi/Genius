bbd2ca3e9c97785e897beb78f72ebf9eb3e8da14
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-26 00:54:11 +0330

    implementing Artist constructor.

--- src/main/java/org/example/Artist.java
+++ src/main/java/org/example/Artist.java
@@ -1,4 +1,8 @@
 package org.example;
 
-public class Artist {
+public class Artist extends Account{
+
+    public Artist(String name, int age, String email, String username, String password, int role) {
+        super(name, age, email, username, password, role);
+    }
 }
