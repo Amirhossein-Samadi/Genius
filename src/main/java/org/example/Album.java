c015f35c112242f24a7bba5355c1c0c006c18ee8
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-26 01:44:06 +0330

    implementing Album constructor.

--- src/main/java/org/example/Album.java
+++ src/main/java/org/example/Album.java
@@ -11,5 +11,12 @@ public class Album {
 
     private ArrayList<String> trackList;
 
+    public Album(String title, String artist, String releaseDate, ArrayList<String> trackList) {
+        this.title = title;
+        this.artist = artist;
+        this.releaseDate = releaseDate;
+        this.trackList = trackList;
+    }
+
 
 }
