b3da8a3fbd3efd6bb0fb70fdf90729d566e136c0
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-26 00:52:14 +0330

    Creating User constructor.

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,8 @@
 package org.example;
 
-public class User {
+public class User extends Account{
+
+    public User(String name, int age, String email, String username, String password, int role) {
+        super(name, age, email, username, password, role);
+    }
 }
