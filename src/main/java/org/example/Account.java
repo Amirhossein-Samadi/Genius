eaf0e9b1d55ff3740ab593441c9afc9c00701d7a
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-26 00:48:39 +0330

    Creating Account class and implement its constructor.

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -1,4 +1,21 @@
 package org.example;
 
-public class Account {
+public abstract class Account {
+
+    private String name;
+    private int age;
+    private String email;
+    private String username;
+    private String password;
+    private int role;
+
+    // implementing Account constructor.
+    public Account(String name, int age, String email, String username, String password, int role) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
 }
