88e756eb171ec0acb4831871ec1d8d62e11f5211
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-26 01:46:05 +0330

    Adding methods to Album.

--- src/main/java/org/example/Album.java
+++ src/main/java/org/example/Album.java
@@ -3,7 +3,6 @@ package org.example;
 import java.util.ArrayList;
 
 public class Album {
-    // title, release date, and tracklist.
 
     private String title;
     private String artist;
@@ -17,6 +16,14 @@ public class Album {
         this.releaseDate = releaseDate;
         this.trackList = trackList;
     }
+
+    public String albumGetTitle() {return title;}
+
+    public String albumGetArtist() {return artist;}
+
+    public String albumGetReleaseDate() {return releaseDate;}
+
+    public ArrayList<String> albumGetTrackList() {return trackList;}
 
 
 }
