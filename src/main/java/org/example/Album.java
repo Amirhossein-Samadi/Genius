e7b2ce686c37add06ddf74420fd2277787abe2ae
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-26 01:42:28 +0330

    Updating Album class.

--- src/main/java/org/example/Album.java
+++ src/main/java/org/example/Album.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Album {
+    // title, release date, and tracklist.
+
+    private String title;
+    private String artist;
+    private String releaseDate;
+
+    private ArrayList<String> trackList;
+
+
 }
