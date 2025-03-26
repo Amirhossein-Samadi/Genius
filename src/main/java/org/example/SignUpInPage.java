0ae6f07124e379fdd0f8baaf401f7efae81d9b20
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-26 13:16:59 +0330

    Updating singupin page.

--- src/main/java/org/example/SignUpInPage.java
+++ src/main/java/org/example/SignUpInPage.java
@@ -1,17 +1,23 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class SignUpInPage extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
     @Override
     public void start(Stage stage) throws Exception {
-        launch();
+         showSignUpInPage();
     }
 
     public static void showSignUpInPage() {
@@ -20,17 +26,25 @@ public class SignUpInPage extends Application {
         stage.setTitle("Sign-up(In) Page");
 
         GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(50));
+
+        Font labeFont = Font.font("Verdana", FontWeight.BOLD, 18);
 
         Label signInLabel = new Label("if you have an Account.");
         Label signUpLabel = new Label("if you don't have an Account.");
 
+        signUpLabel.setFont(labeFont);
+        signInLabel.setFont(labeFont);
+
         Button signInButton = new Button("Sign In");
         Button signUpButton = new Button("Sign Up");
 
+        Font buttonFont = Font.font("Verdana", FontWeight.BOLD, 18);
+
         gridPane.add(signInLabel, 0, 0);
-        gridPane.add(signInButton, 1, 0);
-        gridPane.add(signUpLabel, 0, 1);
-        gridPane.add(signUpButton, 1, 1);
+        gridPane.add(signInButton, 0, 1);
+        gridPane.add(signUpLabel, 0, 2);
+        gridPane.add(signUpButton, 0, 3);
 
         Scene scene = new Scene(gridPane, 400, 400);
         stage.setScene(scene);
