c8ce043cc57913266c7bbfda9d570e3c11677558
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-27 17:33:27 +0330

    Adding songlist and albumlist to Artist.

--- src/main/java/org/example/Artist.java
+++ src/main/java/org/example/Artist.java
@@ -1,8 +1,17 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Artist extends Account{
+
+    private ArrayList<Song> songsList = new ArrayList<>();
+    private ArrayList<Album> albumsList = new ArrayList<>();
 
     public Artist(String name, int age, String email, String username, String password, int role) {
         super(name, age, email, username, password, role);
     }
+
+    public ArrayList<Song> getSongsList() {return songsList;};
+    public ArrayList<Album> getAlbumsList() {return albumsList;};
+    
 }
