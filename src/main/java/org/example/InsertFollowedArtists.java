193de210a32b35432fae9dee9949985f575d0516
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-27 23:47:30 +0330

    Creating InsertFollowed artist

---
+++ src/main/java/org/example/InsertFollowedArtists.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class InsertFollowedArtists {
+
+    public static void insertFollowedArtistsTable(Connection conn, String username, String artistname) throws SQLException {
+
+        String sql = "INSERT INTO ArtistsData(username,artistname) VALUES(?,?)";
+
+        PreparedStatement ps = conn.prepareStatement(sql);
+
+        ps.setString(1, username);
+        ps.setString(2, artistname);
+
+        ps.executeUpdate();
+    }
+}
