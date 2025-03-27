a4aebf5fe24a0e9e041347011f6e4ea076f58735
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-27 17:15:11 +0330

    Adding Followed Artist list to User,

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 
 public class User extends Account{
 
+    private ArrayList<Artist> followedArtist;
+
     public User(String name, int age, String email, String username, String password, int role) {
         super(name, age, email, username, password, role);
     }
+
+    public ArrayList<Artist> getFollowedArtist() {return followedArtist;}
 }
