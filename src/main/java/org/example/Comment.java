783887eb2c54c44c0ed37729de883a647f9b1995
Amirhossein Samadi <bookam2025@gmail.com>
2025-03-27 18:04:14 +0330

    Updating comment.

--- src/main/java/org/example/Comment.java
+++ src/main/java/org/example/Comment.java
@@ -8,15 +8,18 @@ public class Comment {
     private String comment;
     private String author;
     private String date;
+    private String songTitle
 
-    public Comment(String comment, String author, String date) {
+    public Comment(String comment, String author, String date, String songTitle) {
         this.comment = comment;
         this.author = author;
         this.date = date;
+        this.songTitle = songTitle;
     }
 
     public String getComment() {return comment;}
     public String getAuthor() {return author;}
     public String getDate() {return date;}
-    
+    public String getSongTitle() {return songTitle;}
+
 }
