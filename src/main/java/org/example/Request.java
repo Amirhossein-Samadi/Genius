d41fe1ea622099d0428c78cb9e9ba3fc23070167
Amirhossein Samadi <bookam2025@gmail.com>
2025-04-02 12:55:51 +0330

    Creating Request class.

---
+++ src/main/java/org/example/Request.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Request {
+
+    private String username;
+    private String artistName;
+    private String songTitle;
+    private String recommendedLyrics;
+    private String description;
+    private String releaseDate;
+
+    public Request(String username, String artistName, String songTitle, String recommendedLyrics, String description)
+    {
+        this.username = username;
+        this.artistName = artistName;
+        this.songTitle = songTitle;
+        this.recommendedLyrics = recommendedLyrics;
+        this.description = description;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getUsername() {return username;}
+    public String getArtistName() {return artistName;}
+    public String getSongTitle() {return songTitle;}
+    public String getRecommendedLyrics() {return recommendedLyrics;}
+    public String getDescription() {return description;}
+    public String getReleaseDate() {return releaseDate;}
+}
