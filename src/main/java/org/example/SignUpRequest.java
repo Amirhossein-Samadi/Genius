6fa71a8c4ed5f26851fd85aa55cebf989f65c8c8
Amirhossein Samadi <bookam2025@gmail.com>
2025-04-06 20:51:49 +0330

    Creating sign up request class.

---
+++ src/main/java/org/example/SignUpRequest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class SignUpRequest {
+
+    private String artistName;
+    private int status;
+    private String date;
+
+    public SignUpRequest(String artistName, int status, String date)
+    {
+        this.artistName = artistName;
+        this.status = status;
+        this.date = date;
+    }
+
+    public String getArtistName() {return artistName;}
+    public int getStatus() {return status;}
+    public String getDate() {return date;}
+    
+}
